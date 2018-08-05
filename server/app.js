355cce96433e0c8e185fa72729f293206c6c081c
maxim <maxim@webro.com.ua>
2018-08-05 14:08:41 +0300

    added a mongoDB connection

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://test:test123456@ds113442.mlab.com:13442/graphql');
+mongoose.connection.once('open', () => {
+	console.log('connected to DB');
+});
 
 app.use('/graphql', graphqlHTTP({
 	schema,
