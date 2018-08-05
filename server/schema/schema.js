948a4ba4f4da54d5fbfbd9bf2e9968fa13185ce9
maxim <maxim@webro.com.ua>
2018-08-05 13:46:55 +0300

    added a new type and enabled GraphiQL

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,41 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+	GraphQLObjectType,
+	GraphQLString,
+	GraphQLSchema,
+	GraphQLID,
+	GraphQLInt
+} = graphql;
+
+//dummy data
+const books = [
+	{name: 'first book', genre: 'sky-fy', id: '1'},
+	{name: 'second book', genre: 'fantasy', id: '2'},
+	{name: 'third book', genre: 'sky-fy', id: '3'}
+];
+
+const authors = [
+	{name: 'first author', age: 44, id: '1'},
+	{name: 'second author', age: 34, id: '2'},
+	{name: 'third author', age: 24, id: '3'},
+];
 
 const BookType = new GraphQLObjectType({
 		name: 'Book',
 		fields: () => ({
-			id: {type: GraphQLString},
+			id: {type: GraphQLID},
 			name: {type: GraphQLString},
 			genre: {type: GraphQLString}
+		})
+	}
+);
+
+const AuthorType = new GraphQLObjectType({
+		name: 'Author',
+		fields: () => ({
+			id: {type: GraphQLID},
+			name: {type: GraphQLString},
+			age: {type: GraphQLInt}
 		})
 	}
 );
@@ -16,9 +45,16 @@ const RootQuery = new GraphQLObjectType({
 	fields: {
 		book: {
 			type: BookType,
-			args: {id: {type: GraphQLString}},
+			args: {id: {type: GraphQLID}},
 			resolve(parent, args) {
-
+				return books.find(item => item.id === args.id)
+			}
+		},
+		author: {
+			type: AuthorType,
+			args: {id: {type: GraphQLID}},
+			resolve(parent, args) {
+				return authors.find(item => item.id === args.id)
 			}
 		}
 	}
